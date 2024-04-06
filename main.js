4810e63e78cea6b8aa7c7ccce1e116f08a8f1f6c
Carlos Campbells Laptop <carlos.g.campbell@gmail.com>
2024-04-06 13:43:40 -0400

    full code war

---
+++ main.js
@@ -0,0 +1,14 @@
+/**
+
+Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
+
+**/
+
+var capitals = function (word) {
+    var caps = [];
+      for(var i = 0; i < word.length; i++) {
+      if(word[i].toUpperCase() == word[i]) caps.push(i);
+    }
+    return caps;
+  };
+console.log(capitals("word"))
